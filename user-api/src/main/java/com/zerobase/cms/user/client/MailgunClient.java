d91e09f3dc54028b5d4576581ca4522338c96a1f
Oh Keun-Hyeop <rmsguqsla@gmail.com>
2022-09-28 16:18:55 +0900

    feat: [00-EMAIL-Connect] mailgun에 대한 email 연동
    무료계정으로 테스트 시 이메일 등록 반드시 필요

---
+++ user-api/src/main/java/com/zerobase/cms/user/client/MailgunClient.java
@@ -0,0 +1,16 @@
+package com.zerobase.cms.user.client;
+
+import com.zerobase.cms.user.client.mailgun.SendMailForm;
+import feign.Response;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.cloud.openfeign.SpringQueryMap;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@FeignClient(name = "mailgun", url = "https://api.mailgun.net/v3/")
+@Qualifier("mailgun")
+public interface MailgunClient {
+
+    @PostMapping("sandbox3d326998496a48faadbd1a17cac7675a.mailgun.org/messages")
+    Response sendEmail(@SpringQueryMap SendMailForm form);
+}
---
+++ user-api/src/test/java/com/zerobase/cms/user/client/service/EmailSendServiceTest.java
@@ -0,0 +1,22 @@
+package com.zerobase.cms.user.client.service;
+
+import com.zerobase.cms.user.client.MailgunClient;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailSendServiceTest {
+
+    @Autowired
+    private MailgunClient mailgunClient;
+
+    @Test
+    public void EmailTest() {
+        // 숙제
+        mailgunClient.sendEmail(null);
+//        String response = emailSendService.sendEmail();
+//        System.out.println(response);
+    }
+
+}
